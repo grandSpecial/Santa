2f1da146ea6e122731134a012eca289602b35b08
Tyler Batten <batten.tyler@gmail.com>
2024-12-11 15:55:13 -0400

    dynamic host

--- main.py
+++ main.py
@@ -48,8 +48,7 @@ async def index_page():
 async def handle_incoming_call(request: Request):
     """Handle incoming call and return TwiML response to connect to Media Stream."""
     response = VoiceResponse()
-    # host = request.headers.get("Host", "")
-    host = 'hi-santa-2d6b7aacf0f1.herokuapp.com'
+    host = request.headers.get("Host", "")
     connect = Connect()
     connect.stream(url=f'wss://{host}/media-stream')
     response.append(connect)
